1e0281c7ff1a6bec83c075bce076173adb40aa13
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-04-19 14:51:02 +0200

    Max + jag ångrade mig lite på min
    fortfarande fult men fungerande

--- lib/max_of_four.rb
+++ lib/max_of_four.rb
@@ -1,7 +1,7 @@
-def min_of_four(num1, num2, num3, num4)
-    if num2 < num1
-        if num3 < num2
-            if num4 < num3
+def max_of_four(num1, num2, num3, num4)
+    if num2 > num1
+        if num3 > num2
+            if num4 > num3
                 return num4
             end
             return num3
--- lib/max_of_three.rb
+++ lib/max_of_three.rb
@@ -1,6 +1,6 @@
-def min_of_three(num1, num2, num3)
-    if num2 < num1
-        if num3 < num2
+def max_of_three(num1, num2, num3)
+    if num2 > num1
+        if num3 > num2
             return num3
         end
         return num2
