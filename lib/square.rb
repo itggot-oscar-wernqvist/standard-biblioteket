c1866ea206f87b30de8d450efe301fd9069891b8
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-04-26 14:27:43 +0200

    tomtomtomtotmotmotmotmtomtomtomtomtomtomtomtotmotmotmtomtomtotmotmotmtomtom
    klar med dem

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,13 @@
+# Public: Squares a number
+#
+# number - Any number.
+#
+# Examples
+#
+#   square(4)
+#   # => 16
+#
+# Returns the square.
 def square(number)
     return number * number
 end    
--- lib/sum_to.rb
+++ lib/sum_to.rb
@@ -1,3 +1,13 @@
+# Public: Finds out the sum of every number up until input number.
+#
+# number - Any number.
+#
+# Examples
+#
+#   sum_to(2)
+#   # => 3
+#
+# Returns the sum of all smaller and equal numbers.
 def sum_to(num) 
     out = 0
     i = 0
